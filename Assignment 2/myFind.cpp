3f9cb3d886d42fa5b7650ab046bd39dc008ba386
bader-waseem <baderwaseem@hotmail.com>
2018-10-10 21:16:44 -0600

    Added comments scan.sh, created myFind.cpp and scan.cpp

---
+++ Assignment 2/myFind.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Bader Abdulwaseem
+UCID: 30023849
+Assignment 2
+Question 2
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
--- Assignment 2/scan.sh
+++ Assignment 2/scan.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-find . -type f -name "*"$1 -printf "%p %s\n"| sort -k2nr | head -$2 | awk '{x += $2; print $1" "$2} END {print "total bytes: " x}'
+# Author: Bader Abbulwaseem
+# UCID: 30023849
+# Assignment 2
+# Question 1
+find . -type f -name "*"$1 -printf "%p %s\n"| sort -k2nr | head -$2 | awk '{x += $2; print $1" "$2} END {print "Total bytes: " x}'
