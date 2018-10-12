27363113eff112db1da4cb3f16406808306c8eb3
bader-waseem <baderwaseem@hotmail.com>
2018-10-11 20:05:03 -0600

    Updated many files, added test input for sum.cpp

--- Assignment 2/sum.cpp
+++ Assignment 2/sum.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <pthread.h>
+#include <cstdlib>
 
-int main() {
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	// Checks to see if the program is being run with the correct inputs
+	if (argc != 3) {
+		cout << "This program takes two arguments in this format: 'input' 'thread count'" << endl;
+		return 1;
+	}
+
+	// Converting the string into integer
+	int numberOfThreads = stoi(argv[2]);
+
+	vector<int> numbersFromInput;
+
+
+
 	return 0;
 }
